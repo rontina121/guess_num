3e8cb30e14079a1d61d6a16f72160c8728eb78b7
Ron <ntust2001@gmail.com>
2021-08-28 22:37:11 +0800

    first version

---
+++ guess_num.py
@@ -0,0 +1,17 @@
+import random
+r = random.randint(1,10)
+i = 0
+while True:
+    num = input("請猜數字:")
+    num = int(num)
+    i = i + 1
+    if num == r:
+        print("你猜對了")
+        print("共猜了",i,"次")
+        break
+    else:
+        if num > r:
+           print("要更小") 
+        else:
+           print("要更大") 
+
